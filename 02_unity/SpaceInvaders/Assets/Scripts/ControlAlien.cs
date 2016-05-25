6c4aade68a84aa0f798674caa7139c8a528e2123
arantzi <arantzister@gmail.com>
2016-05-25 22:14:47 +0200

    Implementado mas marcianos
    Implementado mas marcianos, con diferentes formas, colores y
    puntuaciones.
    Corrección que no empiece de 0 cada vez que se limpia la pantalla.

--- 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
+++ 02_unity/SpaceInvaders/Assets/Scripts/ControlAlien.cs
@@ -8,7 +8,8 @@ public class ControlAlien : MonoBehaviour
 	private GameObject marcador;
 
 	// Por defecto, 100 puntos por cada alien
-	private int puntos = 100;
+	// private int puntos = 100;
+	// Ponemos puntuación diferente por cada Alien
 
 	// Objeto para reproducir la explosión de un alien
 	private GameObject efectoExplosion;
@@ -40,7 +41,22 @@ public class ControlAlien : MonoBehaviour
 			GetComponent<AudioSource> ().Play ();
 
 			// Sumar la puntuación al marcador
-			marcador.GetComponent<ControlMarcador> ().puntos += puntos;
+			// Dependiendo de qué tipo de alien sea le damos más o menos puntuación.
+			// 100 para los de la primera fila, 200 para los de la segunda, 300 para los de la tercera y 400 para los de la cuarta.
+			if (gameObject.tag == "Alien1"){
+				marcador.GetComponent<ControlMarcador> ().puntos += 100;
+			}
+			if (gameObject.tag == "Alien2"){
+				marcador.GetComponent<ControlMarcador> ().puntos += 200;
+			}
+			if (gameObject.tag == "Alien3"){
+				marcador.GetComponent<ControlMarcador> ().puntos += 300;
+			}
+			if (gameObject.tag == "Alien4"){
+				marcador.GetComponent<ControlMarcador> ().puntos += 400;
+			}
+			
+			
 
 			// El disparo desaparece (cuidado, si tiene eventos no se ejecutan)
 			Destroy (coll.gameObject);
@@ -51,6 +67,7 @@ public class ControlAlien : MonoBehaviour
 
 		} else if (coll.gameObject.tag == "nave") {
 			//SceneManager.LoadScene ("Nivel1");
+			//Cargamos la escena de "GameOver"
 			SceneManager.LoadScene ("GameOver");
 		}
 	}
